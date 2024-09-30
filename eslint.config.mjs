28f2f7a8e2621aad49ddea9490180f998148005a
Nelsonrrj <nelsonrodriguezj@gmail.com>
2024-09-30 15:37:04 -0400

    feat(lint): add some eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,12 @@
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(eslintPluginPrettierRecommended)
+export default withNuxt(eslintPluginPrettierRecommended, {
+  rules: {
+    semi: 0,
+    'vue/multi-word-component-names': 'off',
+    '@stylistic/quote-props': 'as-needed',
+    '@stylistic/brace-style': '1tbs',
+    '@stylistic/arrow-parens': 'always',
+  },
+})
