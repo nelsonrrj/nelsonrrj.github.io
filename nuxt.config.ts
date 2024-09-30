8a06d891eb1236b91d024c0664c66ef3f4cab5e2
Nelsonrrj <nelsonrodriguezj@gmail.com>
2024-09-29 21:02:03 -0400

    feat(eslint): install eslint package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,28 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   runtimeConfig: {
     public: {
       enableProjects: false,
       enableBlog: false,
-    }
+    },
+  },
+
+  modules: [
+    '@nuxt/eslint',
+  ],
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
   },
 })
