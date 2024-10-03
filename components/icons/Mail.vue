0b00c218b9785e317bcb5a29653821eff90073ea
Nelsonrrj <nelsonrodriguezj@gmail.com>
2024-10-03 16:53:16 -0400

    feat: updated social media icons

--- components/icons/Mail.vue
+++ components/icons/Mail.vue
@@ -2,19 +2,23 @@
 
 <template>
   <svg
-    aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
-    fill="none"
     viewBox="0 0 24 24"
+    fill="currentColor"
+    class="icon icon-tabler icons-tabler-filled icon-tabler-mail"
   >
     <path
-      stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M11 16v-5.5A3.5 3.5 0 0 0 7.5 7m3.5 9H4v-5.5A3.5 3.5 0 0 1 7.5 7m3.5 9v4M7.5 7H14m0 0V4h2.5M14 7v3m-3.5 6H20v-6a3 3 0 0 0-3-3m-2 9v4m-8-6.5h1"
+      stroke="none"
+      d="M0 0h24v24H0z"
+      fill="none"
+    />
+    <path
+      d="M22 7.535v9.465a3 3 0 0 1 -2.824 2.995l-.176 .005h-14a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-9.465l9.445 6.297l.116 .066a1 1 0 0 0 .878 0l.116 -.066l9.445 -6.297z"
+    />
+    <path
+      d="M19 4c1.08 0 2.027 .57 2.555 1.427l-9.555 6.37l-9.555 -6.37a2.999 2.999 0 0 1 2.354 -1.42l.201 -.007h14z"
     />
   </svg>
 </template>
