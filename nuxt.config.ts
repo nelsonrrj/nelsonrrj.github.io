a1b23c7a86cd88300c5b13667897d4b1a9565112
Nelsonrrj <nelsonrodriguezj@gmail.com>
2024-10-08 17:21:24 -0400

    feat(github): Add configuration to deploy in github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+
   devtools: { enabled: true },
+
   css: ['~/assets/css/main.css'],
 
   postcss: {
@@ -11,12 +13,7 @@ export default defineNuxtConfig({
     },
   },
 
-  runtimeConfig: {
-    public: {
-      enableProjects: false,
-      enableBlog: false,
-    },
-  },
+  runtimeConfig: {},
 
   modules: ['@nuxt/eslint'],
 
